fd6684c91100fa7030a917209da9a80b6536351c
briannalturner <blturner3527@gmail.com>
2020-08-23 19:26:07 -0400

    working on navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,16 @@
 import React from 'react'
-import { gsap } from 'gsap'
+// import { gsap } from 'gsap'
 
 export default class Home extends React.Component {
-
-    constructor() {
-        super()
-        this.box = null;
-        this.boxAnimation = null;
-    }
-
-    componentDidMount() {
-        this.boxAnimation = gsap.to('.moving-orange-box', {
-            x: '300', 
-            duration: '3', 
-            rotation: '360', 
-            ease:'elastic', 
-            yoyo: 'true', 
-            repeat: -1
-        }).pause();
-    };
     
 
     render() {
         
         return(
             <div className='p-5'>
-                <div ref={div => this.box = div} className='bg-warning card moving-orange-box' style={{width:'100px', height:'100px', border: 'none'}}>
+                <div className='bg-primary'>
+                    Hi, I'm Bri
                 </div>
-                <br></br>
-                <button className='btn btn-danger float-left m-1' onClick={() => this.boxAnimation.play()}>Play</button>
-                <button className='btn btn-danger float-left m-1' onClick={() => this.boxAnimation.pause()}>Pause</button>
-                <button className='btn btn-danger float-left m-1' onClick={() => this.boxAnimation.reverse()}>Reverse</button>
-                <button className='btn btn-danger float-left m-1' onClick={() => this.boxAnimation.restart()}>Restart</button>
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import { gsap } from 'gsap'
-import logo from '../pictures/bt2.png'
+// import { gsap } from 'gsap'
+import logo from '../pictures/BT2.png'
+import name from '../pictures/name.png'
 
 export default class Navbar extends React.Component {
 
-    componentDidMount() {
-        gsap.to('.logo', {rotation: '360', repeat: -1, ease: 'none', duration: 5})
-    }
-
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-                <img src={logo} className="mx-2 logo" alt='logo' style={{width: '30px', boxShadow:'black'}} />
-                <NavLink to="/" className="navbar-brand">Bri Turner</NavLink>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                {/* <img src={logo} className="mx-2 logo" alt='logo' style={{width: '40px', boxShadow:'black'}} /> */}
+                <img src={name} alt='name' style={{maxWidth: '125px', boxShadow:'black'}} />
+                <span className='ml-auto'>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <NavLink to='/' className="nav-link" activeClassName="active" >Home</NavLink>
+                            <NavLink to="/portfolio" className="nav-link" activeClassName="active" >Portfolio</NavLink>
+                            <NavLink to="/about" className="nav-link" activeClassName="active" >About Me</NavLink>
+                        </div>
+                    </div>
+                </span>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <NavLink to="/portfolio" className="nav-link" activeClassName="active" >Portfolio</NavLink>
-                        <NavLink to="/about" className="nav-link" activeClassName="active" >About Me</NavLink>
-                    </div>
-                </div>
             </nav>
         )
     }
